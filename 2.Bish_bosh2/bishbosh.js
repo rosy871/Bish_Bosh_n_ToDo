c57297a58ca96ab745ea23d259828c43f8bc9883
rosy871 <yalameju@gmail.com>
2021-08-06 14:48:55 +0200

    small changes

--- 2.Bish_bosh2/bishbosh.js
+++ 2.Bish_bosh2/bishbosh.js
@@ -5,12 +5,23 @@ const bbin=document.querySelector('#bish_bosh_input');
 
 btn.addEventListener('click', function(){
 div1.style.borderStyle='double';
+let res;
 let x = bbin.value;
+if(isNaN(x)||x<1)
+{
+    res='input not valid type... num greater than 0';
+    ta1.innerText=res;
+}
+else
+{
+    res=function2(x);
+    ta1.innerText=res;
+
+}
+
 console.log(x);
 bbin.value='';
-let res=function2(x);
 
-ta1.innerText=res;
 
 })
 
@@ -19,13 +30,13 @@ function function2(bbvalue)
     let output,result='';
     for (let num = 1; num <= bbvalue; num++) 
     {
-      if(num%3==0&&num%4!=0)
+      if(num%3===0&&num%4!==0)
           output='Bish';
   
-      else if(num%4==0&&num%3!=0)
+      else if(num%4===0&&num%3!==0)
           output='Bosh'
   
-      else if(num%3==0&&num%4==0)
+      else if(num%3===0&&num%4===0)
           output='Bish-Bosh';
   
       else
